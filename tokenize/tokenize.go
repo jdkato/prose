acf07834852b4c6611dbc92a5b782d43479cdb81
Joseph Kato <joseph@jdkato.io>
2017-03-03 10:52:25 -0800

    feat: add character identification helpers

--- tokenize/tokenize.go
+++ tokenize/tokenize.go
@@ -17,3 +17,33 @@ type WordTokenizer interface {
 type SentenceTokenizer interface {
 	Tokenize(text string) []string
 }
+
+// IsPunct determines if a character is a punctuation symbol.
+func IsPunct(c byte) bool {
+	for _, r := range []byte("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~") {
+		if c == r {
+			return true
+		}
+	}
+	return false
+}
+
+// IsSpace determines if a character is a whitespace character.
+func IsSpace(c byte) bool {
+	for _, r := range []byte("\t\n\r\f\v") {
+		if c == r {
+			return true
+		}
+	}
+	return false
+}
+
+// IsLetter determines if a character is letter.
+func IsLetter(c byte) bool {
+	return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+}
+
+// IsAlnum determines if a character is a letter or a digit.
+func IsAlnum(c byte) bool {
+	return (c >= '0' && c <= '9') || IsLetter(c)
+}
