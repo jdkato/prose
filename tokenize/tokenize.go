5bf5c391907b29eb68f49fb02fa55bf1311ebaf0
Joseph Kato <joseph@jdkato.io>
2017-04-05 15:54:55 -0700

    tokenize: Add TextToWords
    This provides a convenient wrapper around sentence and word
    tokenization.

---
+++ tokenize/tokenize.go
@@ -0,0 +1,27 @@
+/*
+Package tokenize implements functions to slice UTF-8 encoded strings.
+*/
+package tokenize
+
+import (
+	"github.com/jdkato/prose/internal/util"
+	"gopkg.in/neurosnap/sentences.v1/english"
+)
+
+// TextToWords converts the string text into a slice of words.
+//
+// It does so by tokenizing text into sentences (using a port of NLTK's punkt
+// tokenizer; see https://github.com/neurosnap/sentences) and then tokenizing
+// the sentences into words via TreebankWordTokenizer.
+func TextToWords(text string) []string {
+	sentTokenizer, err := english.NewSentenceTokenizer(nil)
+	util.CheckError(err)
+	wordTokenizer := NewTreebankWordTokenizer()
+
+	words := []string{}
+	for _, s := range sentTokenizer.Tokenize(text) {
+		words = append(words, wordTokenizer.Tokenize(s.Text)...)
+	}
+
+	return words
+}
