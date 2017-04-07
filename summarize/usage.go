ae541f2f9e679ea5f4a34a19f7b0645b1776da15
Joseph Kato <joseph@jdkato.io>
2017-04-07 11:24:08 -0700

    summarize: Add WordDensity

--- summarize/usage.go
+++ summarize/usage.go
@@ -1,1 +1,10 @@
 package summarize
+
+// WordDensity returns a map of each word and its density in the Document d.
+func (d *Document) WordDensity() map[string]float64 {
+	density := make(map[string]float64)
+	for word, count := range d.WordFrequency {
+		density[word] = float64(count) / d.NumWords
+	}
+	return density
+}
