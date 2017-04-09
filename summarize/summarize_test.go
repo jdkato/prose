62ed4539088d56fd7cc52851cc6b628c93bf2986
Joseph Kato <joseph@jdkato.io>
2017-04-09 10:27:06 -0700

    summarize: Move syllable testing to its own file

--- summarize/summarize_test.go
+++ summarize/summarize_test.go
@@ -6,43 +6,8 @@ import (
 	"testing"
 
 	"github.com/jdkato/prose/internal/util"
-	"github.com/jdkato/syllables"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestSyllables(t *testing.T) {
-	cases := util.ReadDataFile(filepath.Join(testdata, "syllables.json"))
-	tests := make(map[string]int)
-	util.CheckError(json.Unmarshal(cases, &tests))
-
-	for word, count := range tests {
-		assert.Equal(t, count, Syllables(word), word)
-	}
-}
-
-func BenchmarkSyllables(b *testing.B) {
-	cases := util.ReadDataFile(filepath.Join(testdata, "syllables.json"))
-	tests := make(map[string]int)
-	util.CheckError(json.Unmarshal(cases, &tests))
-
-	for n := 0; n < b.N; n++ {
-		for word := range tests {
-			Syllables(word)
-		}
-	}
-}
-
-func BenchmarkSyllablesIn(b *testing.B) {
-	cases := util.ReadDataFile(filepath.Join(testdata, "syllables.json"))
-	tests := make(map[string]int)
-	util.CheckError(json.Unmarshal(cases, &tests))
-
-	for n := 0; n < b.N; n++ {
-		for word := range tests {
-			syllables.In(word)
-		}
-	}
-}
 
 type testCase struct {
 	Text       string
