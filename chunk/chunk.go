4b7d767cc83be692ed5d2ce6a306cb22c39389c8
Elliott Stoneham <elliott.stoneham@gmail.com>
2017-04-13 20:16:24 +0100

    Allow cardinal numbers within NamedEntities (rather than just at the end)

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -35,8 +35,8 @@ func quadsString(tagged []tag.Token) string {
 // TreebankNamedEntities matches proper names, including prior adjectives,
 // possibly including a linkage by preposition or subordinating conjunctions (for example "Bank of England").
 var TreebankNamedEntities = regexp.MustCompile(
-	`((JJ._)*(NNP.)+(CD__)*)+` + // at least one proper noun, maybe preceeded by an adjective
-		`((IN__)*(JJ._)*(NNP.)+(CD__)*)*`) // then zero or more related noun phrases
+	`((JJ._)*(NNP.)+(CD__)*(NNP.)*)+` + // at least one proper noun, maybe preceeded by an adjective
+		`((IN__)*(JJ._)*(NNP.)+(CD__)*(NNP.)*)*`) // then zero or more related noun phrases
 
 // Locate the chunks of interest according to the regexp.
 func Locate(tagged []tag.Token, rx *regexp.Regexp) [][]int {
