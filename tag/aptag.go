80fa9da1cf465f71175752293aaf6f2e5500302d
Joseph Kato <joseph@jdkato.io>
2017-05-09 10:04:07 -0700

    Update licenses

--- tag/aptag.go
+++ tag/aptag.go
@@ -1,5 +1,5 @@
 // Copyright 2013 Matthew Honnibal
-
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
@@ -9,7 +9,7 @@
 
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
