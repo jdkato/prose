3d144dfc7fc1ce7b55dc608c730b88aa908dc015
Joseph Kato <joseph@jdkato.io>
2017-05-10 13:45:52 -0700

    Fix incorrect reference to TreebankNamedEntities
    I meant to pass `rx` (the parameter)

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -48,7 +48,7 @@ var TreebankNamedEntities = regexp.MustCompile(
 // need access the to the in-text locations of each chunk.
 func Chunk(tagged []tag.Token, rx *regexp.Regexp) []string {
 	chunks := []string{}
-	for _, loc := range Locate(tagged, TreebankNamedEntities) {
+	for _, loc := range Locate(tagged, rx) {
 		res := ""
 		for t, tt := range tagged[loc[0]:loc[1]] {
 			if t != 0 {
