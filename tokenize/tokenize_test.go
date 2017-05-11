21c571f0251c775c3a34d214f0768bd1f68db022
Joseph Kato <joseph@jdkato.io>
2017-05-11 09:49:56 -0700

    Fix example tests

--- tokenize/tokenize_test.go
+++ tokenize/tokenize_test.go
@@ -1,13 +1,21 @@
 package tokenize
 
+import "fmt"
+
 func ExampleNewWordBoundaryTokenizer() {
 	t := NewWordBoundaryTokenizer()
-	t.Tokenize("They'll save and invest more.")
+	fmt.Println(t.Tokenize("They'll save and invest more."))
 	// Output: [They'll save and invest more]
 }
 
 func ExampleNewWordPunctTokenizer() {
 	t := NewWordPunctTokenizer()
-	t.Tokenize("They'll save and invest more.")
+	fmt.Println(t.Tokenize("They'll save and invest more."))
+	// Output: [They ' ll save and invest more .]
+}
+
+func ExampleNewTreebankWordTokenizer() {
+	t := NewTreebankWordTokenizer()
+	fmt.Println(t.Tokenize("They'll save and invest more."))
 	// Output: [They 'll save and invest more .]
 }
