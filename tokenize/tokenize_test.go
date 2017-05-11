35f5a6975907e3493df5949cee9b620cb9283234
Joseph Kato <joseph@jdkato.io>
2017-05-11 10:05:55 -0700

    Add TestTextToWords

--- tokenize/tokenize_test.go
+++ tokenize/tokenize_test.go
@@ -1,6 +1,11 @@
 package tokenize
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func ExampleNewWordBoundaryTokenizer() {
 	t := NewWordBoundaryTokenizer()
@@ -18,4 +23,17 @@ func ExampleNewTreebankWordTokenizer() {
 	t := NewTreebankWordTokenizer()
 	fmt.Println(t.Tokenize("They'll save and invest more."))
 	// Output: [They 'll save and invest more .]
+}
+
+func TestTextToWords(t *testing.T) {
+	text := "Vale is a natural language linter that supports plain text, markup (Markdown, reStructuredText, AsciiDoc, and HTML), and source code comments. Vale doesn't attempt to offer a one-size-fits-all collection of rules—instead, it strives to make customization as easy as possible."
+	expected := []string{
+		"Vale", "is", "a", "natural", "language", "linter", "that", "supports",
+		"plain", "text", ",", "markup", "(", "Markdown", ",", "reStructuredText",
+		",", "AsciiDoc", ",", "and", "HTML", ")", ",", "and", "source", "code",
+		"comments", ".", "Vale", "does", "n't", "attempt", "to", "offer", "a",
+		"one-size-fits-all", "collection", "of", "rules—instead", ",", "it",
+		"strives", "to", "make", "customization", "as", "easy", "as", "possible",
+		"."}
+	assert.Equal(t, expected, TextToWords(text))
 }
