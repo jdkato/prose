882a9937366cf30941101672bd8620827cdfbac3
Joseph Kato <joseph@jdkato.io>
2017-10-03 10:33:13 -0700

    Extend the `transform` package
    Adds `SimpleCase`, `SnakeCase`, and `DashCase`.

--- transform/transform.go
+++ transform/transform.go
@@ -2,3 +2,43 @@
 Package transform implements functions to manipulate UTF-8 encoded strings.
 */
 package transform
+
+import (
+	"regexp"
+	"strings"
+	"unicode"
+)
+
+var spaces = regexp.MustCompile(" +")
+
+func removeCase(s string, sep string) string {
+	out := ""
+	old := ' '
+	for i, c := range s {
+		alpha := unicode.IsLetter(c) || unicode.IsNumber(c)
+		mat := (i > 1 && unicode.IsLower(old) && unicode.IsUpper(c))
+		if mat || !alpha || (unicode.IsSpace(c) && c != ' ') {
+			out += " "
+		}
+		if alpha || c == ' ' {
+			out += string(unicode.ToLower(c))
+		}
+		old = c
+	}
+	return spaces.ReplaceAllString(strings.TrimSpace(out), sep)
+}
+
+// SimpleCase returns a space-separated, lower-cased copy of the string s.
+func SimpleCase(s string) string {
+	return removeCase(s, " ")
+}
+
+// DashCase returns a dash-separated, lower-cased copy of the string s.
+func DashCase(s string) string {
+	return removeCase(s, "-")
+}
+
+// SnakeCase returns a underscore-separated, lower-cased copy of the string s.
+func SnakeCase(s string) string {
+	return removeCase(s, "_")
+}
--- transform/transform_test.go
+++ transform/transform_test.go
@@ -1,11 +1,35 @@
 package transform
 
 import (
+	"encoding/json"
 	"fmt"
 	"path/filepath"
+	"testing"
+
+	"github.com/jdkato/prose/internal/util"
+	"github.com/stretchr/testify/assert"
 )
 
 var testdata = filepath.Join("..", "testdata")
+
+type testFormat struct {
+	Input string
+	None  string
+	Snake string
+	Param string
+}
+
+func TestTransform(t *testing.T) {
+	tests := make([]testFormat, 0)
+	cases := util.ReadDataFile(filepath.Join(testdata, "case.json"))
+
+	util.CheckError(json.Unmarshal(cases, &tests))
+	for _, test := range tests {
+		assert.Equal(t, test.None, SimpleCase(test.Input))
+		assert.Equal(t, test.Snake, SnakeCase(test.Input))
+		assert.Equal(t, test.Param, DashCase(test.Input))
+	}
+}
 
 func ExampleNewTitleConverter() {
 	tc := NewTitleConverter(APStyle)
