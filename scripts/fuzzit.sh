338fb9452b8e17e12755d9dd5e1d4dd5770290a5
Joseph Kato <joseph@jdkato.io>
2019-08-13 13:28:23 -0700

    Use $FUZZIT_API_KEY

--- scripts/fuzzit.sh
+++ scripts/fuzzit.sh
@@ -20,8 +20,9 @@ function target {
 go get -u github.com/dvyukov/go-fuzz/go-fuzz-build
 wget -q -O fuzzit https://github.com/fuzzitdev/fuzzit/releases/download/v2.4.12/fuzzit_Linux_x86_64
 chmod a+x fuzzit
+
+./fuzzit auth $FUZZIT_API_KEY
 if [ "$1" = "fuzzing" ]; then
-    ./fuzzit auth $FUZZIT_API_KEY
     export LOCAL=""
 else
     export LOCAL="--local"
