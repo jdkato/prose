84bca07214fcf78b7c2b2544aae9a2e982b9f1d0
Benjamin Ackerman <ben@tacmail.net>
2019-09-27 17:06:31 -0700

    Fixing "TODO: look into more efficient lookup techniques." (#47)
    * adding SliceToMap in util
    
    * Completed TODO and replaced StringInSlice with more efficient method.  Old benchmark speed 20975040 ns/op, new benchmark speed 179540 ns/op
    
    * Replacing expensive StringInSlice call with map lookup when the StringInSlice is being called repetitively
    
    * changing stopWords and easyWords to be maps instead of slices

--- summarize/easy_test.go
+++ summarize/easy_test.go
@@ -8,14 +8,16 @@ import (
 	"github.com/jdkato/prose/internal/util"
 )
 
-func BenchmarkEasyWordsLookup(b *testing.B) {
+func BenchmarkEasyWordsLookupMap(b *testing.B) {
 	cases := util.ReadDataFile(filepath.Join(testdata, "syllables.json"))
 	tests := make(map[string]int)
 	util.CheckError(json.Unmarshal(cases, &tests))
 
 	for n := 0; n < b.N; n++ {
 		for word := range tests {
-			util.StringInSlice(word, easyWords)
+			if _, ok := easyWords[word]; ok {
+
+			}
 		}
 	}
 }
