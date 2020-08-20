f43b965a8812171204e98db8420e3b2daf0d3147
Joseph Kato <joseph@jdkato.io>
2020-08-19 17:00:17 -0700

    Fix test name
    Tests need to be prefixed with 'Test' or 'Benchmark'.

--- document_test.go
+++ document_test.go
@@ -17,13 +17,13 @@ func BenchmarkDoc(b *testing.B) {
 	}
 }
 
-func CustomTokenizer(b *testing.B) {
+func BenchmarkCustomTokenizer(b *testing.B) {
 	content := readDataFile(filepath.Join(testdata, "sherlock.txt"))
-    tok := NewIterTokenizer(
-        UsingSanitizer(strings.NewReplacer()), // Disable sanitizer
-        UsingPrefixes([]string{"(", `"`, "[", "'"}),
-        UsingSuffixes([]string{",", ")", `"`, "]", "!", ";", ".", "?", ":", "'"}),
-    )
+	tok := NewIterTokenizer(
+		UsingSanitizer(strings.NewReplacer()), // Disable sanitizer
+		UsingPrefixes([]string{"(", `"`, "[", "'"}),
+		UsingSuffixes([]string{",", ")", `"`, "]", "!", ";", ".", "?", ":", "'"}),
+	)
 	text := string(content)
 	for n := 0; n < b.N; n++ {
 		_, err := NewDocument(text, UsingTokenizer(tok))
